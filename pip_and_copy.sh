9e81f0a6d53f2a35ed0f6772712db19fe6486226
nonbeing <anattaspace@gmail.com>
2025-10-07 10:08:29 +0530

    Minor echo update

--- pip_and_copy.sh
+++ pip_and_copy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# this script is used in and by build.sh
+# this script is invoked by build.sh
 PKG_DIR=$1
 LIB_DIR=$2
 
+echo "pip install requirements.txt to '${PKG_DIR}'..."
 pip install -r requirements.txt -t ${PKG_DIR};
 
 for i in `ls /usr/lib64/mysql/libmysqlclient.so*`;
@@ -12,7 +13,7 @@ do
     then
         # only copy libmysqlclient.so.21, NOT libmysqlclient.so or libmysqlclient.so.21.1.20
         # because libmysqlclient.so.21 is the necessary and sufficient file for mysqlclient to work
-        echo "COPYING '$i' to output dir..."
+        echo "COPYING '$i' to output dir '${LIB_DIR}'..."
         cp $i ${LIB_DIR}
     fi
 done
